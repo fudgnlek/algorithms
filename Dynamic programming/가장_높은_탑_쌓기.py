bca12f63d2da50fe034089b54f1a292ccf0fecd5
fudgnlek <108926789+fudgnlek@users.noreply.github.com>
2023-07-03 13:36:35 +0900

    Add Feedback

--- Dynamic programming/가장_높은_탑_쌓기.py
+++ Dynamic programming/가장_높은_탑_쌓기.py
@@ -16,3 +16,8 @@ for i in range(n):
         if arr[i][2]<arr[j][2]:
             dp[i]=max(dp[i],dp[j]+arr[i][1])
 print(max(dp))
+
+
+# Feedback     
+# 무거운 것, 넓이 큰 것 먼저 쌓아야해서 내림차순 정렬하고 대소비교    
+# -> 위에 올라가는 것이 더 가벼워야함     
